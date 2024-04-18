87a264f6a51c58979632364144a706a5e9997740
Andrew MacIsaac <macisaac.andrew@gmail.com>
2024-04-18 12:44:55 -0700

    feat: Add VS Codium as primary text editor
    Also install the sync extension to update/install other features.

--- users/awm/developer.nix
+++ users/awm/developer.nix
@@ -25,6 +25,20 @@
 
   # Additional packages.
   home.packages = with pkgs; [
-    lapce # Text editor
+    lapce # Experimental text editor
   ];
+
+  # Install VS Codium.
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscodium.fhs;
+    extensions = pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "sync-settings";
+        publisher = "zokugun";
+        version = "0.16.0";
+        sha256 = "97b4b3bf8b6d5e8c31b86becb11cef7a5eaf5fcb950f827b0e55c9efd23b6fc4";
+      }
+    ];
+  };
 }
