2ee46915823b148ed3ab95f6f4f4d6fdd3133d45
Andrew MacIsaac <macisaac.andrew@gmail.com>
2025-06-29 10:49:51 -0700

    feat: Discover network printers

--- systems/poseidon.nix
+++ systems/poseidon.nix
@@ -26,6 +26,14 @@
     ];
   };
 
+  # Network printers.
+  services.printing.enable = true;
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
+
   ### Disk Setup ###
 
   # Encrypted boot devices.
